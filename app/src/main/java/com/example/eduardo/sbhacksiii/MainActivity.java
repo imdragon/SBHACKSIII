ec28f4338359eb0c1b63c47e5636f0fbafbe4b22
Eduardo <i.m.dragon@gmail.com>
2017-01-20 23:38:28 -0800

    Initial commit

---
+++ app/src/main/java/com/example/eduardo/sbhacksiii/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.eduardo.sbhacksiii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = (MapView) findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+        mMapView.setMap(map);
+    }
+}
